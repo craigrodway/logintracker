61686361375f7c455aa639003ce8ed714acc0673
Craig Rodway <craig.rodway@gmail.com>
2013-01-31 19:52:25 +0000

    Emptied contents of master branch after moving files to v2 branch.

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("api/inc/init.php");
-
-$interface = fRequest::getValid('ui', array('normal', 'mobile'));
-define('BASE_URL', preg_replace('/api\//', '', URL_ROOT) . $interface . '/');
-
-require "$interface/layout.php";
